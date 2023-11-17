39dabdececf96bb33df945db09ab241f9ec7a8ef
rmitos <mobileapps@rmitsolutions.net>
2023-11-17 11:15:20 +0530

    Updated provider class.

--- documentscannerandroid/src/main/java/com/scanlibrary/ScanFileProvider.java
+++ documentscannerandroid/src/main/java/com/scanlibrary/ScanFileProvider.java
@@ -2,7 +2,8 @@ package com.scanlibrary;
 
 import androidx.core.content.FileProvider;
 
-class ScanFileProvider extends FileProvider {
+public class ScanFileProvider extends FileProvider {
    //extend fileprovider to stop collision with camera gps timestamp library file provider
    //this class is empty and used in manifest
+    //public ScanFileProvider() {}
 }
